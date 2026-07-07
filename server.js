c869e516bbcee34e5989039cbd95d18e856508e6
Haytham Lachguer <hlachguer@dynamicecohome.com>
2026-07-07 11:51:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ import { loadKnowledge, logChatEvent, publicPath, saveLead } from "./storage.js"
 await loadEnv();
 
 const port = Number(process.env.PORT || 3001);
-const host = process.env.HOST || "127.0.0.1";
+const host = process.env.HOST || "0.0.0.0";
 const supportEmail = process.env.SUPPORT_EMAIL || DEFAULT_SUPPORT_EMAIL;
 const supportPhone = process.env.SUPPORT_PHONE || DEFAULT_SUPPORT_PHONE;
 const publicDir = publicPath();
